09aff4e604a65fb302f7da5e299f8ef68612bb6c
SokyranTheDragon <exploder1531@gmail.com>
2025-01-04 01:01:34 +0100

    Updated brush netherite smithing .json and datagen
    Using the vanilla `netheriteSmithing` method will create recipe unlock advancment referencing the recipe itself as `minecraft:<item_name>_smithing`, rather than using proper ID.
    
    This also updates the .json for recipe unlock advancment as well so it properly references the recipe.

--- fabric/src/main/java/com/github/sokyranthedragon/slightlyrebrushed/datagen/fabric/SlightlyRebrushedRecipeGenerator.java
+++ fabric/src/main/java/com/github/sokyranthedragon/slightlyrebrushed/datagen/fabric/SlightlyRebrushedRecipeGenerator.java
@@ -5,11 +5,16 @@ import com.github.sokyranthedragon.slightlyrebrushed.items.SlightlyRebrushedItem
 import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricRecipeProvider;
 import net.minecraft.core.HolderLookup;
+import net.minecraft.core.registries.Registries;
 import net.minecraft.data.recipes.RecipeCategory;
 import net.minecraft.data.recipes.RecipeOutput;
 import net.minecraft.data.recipes.RecipeProvider;
+import net.minecraft.data.recipes.SmithingTransformRecipeBuilder;
+import net.minecraft.resources.ResourceKey;
 import net.minecraft.tags.ItemTags;
+import net.minecraft.world.item.Item;
 import net.minecraft.world.item.Items;
+import net.minecraft.world.item.crafting.Ingredient;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.concurrent.CompletableFuture;
@@ -35,6 +40,17 @@ public class SlightlyRebrushedRecipeGenerator extends RecipeProvider
             .save(output);
 
         netheriteSmithing(SlightlyRebrushedItems.DIAMOND_BRUSH.get(), RecipeCategory.TOOLS, SlightlyRebrushedItems.NETHERITE_BRUSH.get());
+    }
+
+    @Override
+    public void netheriteSmithing(Item item, RecipeCategory recipeCategory, Item item2)
+    {
+        // Replace vanilla netheriteSmithing method, as its recipe unlock advancment will always
+        // reference "minecraft:<item_name>_smithing" as the recipe, rather than using proper ID.
+        SmithingTransformRecipeBuilder
+            .smithing(Ingredient.of(Items.NETHERITE_UPGRADE_SMITHING_TEMPLATE), Ingredient.of(item), this.tag(ItemTags.NETHERITE_TOOL_MATERIALS), recipeCategory, item2)
+            .unlocks("has_netherite_ingot", has(ItemTags.NETHERITE_TOOL_MATERIALS))
+            .save(output, ResourceKey.create(Registries.RECIPE, SlightlyRebrushedMod.resourceLocation(getItemName(item2) + "_smithing")));
     }
 
     public static class RebrushedRecipeRunner extends FabricRecipeProvider
